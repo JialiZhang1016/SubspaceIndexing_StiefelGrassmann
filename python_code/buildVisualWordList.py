6a60c838b0397c56fd01567291659888dae9aea2
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-08-03 16:00:45 -0500

    Add files via upload

--- python_code/buildVisualWordList.py
+++ python_code/buildVisualWordList.py
@@ -27,20 +27,18 @@ def buildVisualWordList(x, ht):
     
     # turn x into an array of dimension n times d, x is an array
     x = np.array(x)
-    print("x=", x)
-    print("ht=", ht)
+    #print("x=", x)
+    #print("ht=", ht)
     # var
     n = len(x)
-    print("n=", n)
+    #print("n=", n)
     kd = len(x[0])
     nNode = 2**(ht+1) - 1 
     nLeafNode = 2**ht
         
     # intermediate storages, offs is a list of lists
-    offs = []
-    for i in range(nNode):
-        offs.append([])
-    print("offs=", offs)
+    offs = [[] for _ in range(nNode)]
+    #print("offs=", offs)
     
     # initialize indx as a dictionary {"d_cuts": [store cut dimensions], "v_cuts": [store cut values]}
     indx = {"d_cuts": [], "v_cuts": []}
@@ -52,42 +50,66 @@ def buildVisualWordList(x, ht):
     soffs, sv = zip(*sorted(enumerate(x.T[0]), key=itemgetter(1))) 
     sv = list(sv)
     soffs = list(soffs)
-    print("sv=", sv)
-    print("soffs=", soffs)
+    #print("sv=", sv)
+    #print("soffs=", soffs)
     indx['v_cuts'].append(sv[math.floor(n/2)-1])
-    print("indx=", indx)
+    #print("indx=", indx)
     
     # split at mv, left child x(:,1) <= mv, right child x(:,1) > mv
-    offs[1]=soffs[1-1 : math.floor(n/2)-1] 
-    offs[2]=soffs[math.floor(n/2)+1-1 : n-1]
-    print("offs=", offs)
+    offs[1]=soffs[1-1 : math.floor(n/2)] 
+    offs[2]=soffs[math.floor(n/2)+1-1 : n]
+    #print("offs=", offs)
     
     for h in range(ht-1):
-        print("h=", h)
+        #print("h=", h)
         # parents nodes at height h+1
         for k in range(2**(h+1), 2**(h+1+1)):
-            print("k=", k)
+            #print("k=", k)
             # compute covariance
             offs_k = offs[k-1]
-            print("offs_k=", offs_k)
+            #print("offs_k=", offs_k)
             nk = len(offs_k)
             # median offs 
-            moffs = math.floor(nk/2-1) 
+            moffs = math.floor(nk/2)-1 
             # pick all rows of x with indexes from offs_k
-            x_offs_k = []
-            for i in offs_k:
-                x_offs_k.append(x[i])
-            print("x_offs_k=", x_offs_k)
-            # compute the covariance of x along the dimensions in offs_k, find the dimension of the maximal variance
-            sk = np.var(x_offs_k, 1);
-            print("sk=", sk)
+            x_offs_k = [x[_] for _ in offs_k]
+            x_offs_k = np.array(x_offs_k)
+            #print("x_offs_k=", x_offs_k)
+            # compute the covariance of x in offs_k along the kd dimensions, find the dimension with the maximal variance
+            sk = np.var(x_offs_k, 0);
+            #print("sk=", sk)
             max_s = max(sk)
             sk = list(sk)
-            d_cut_index = sk.index(max(sk))
-            d_cut = offs_k[d_cut_index]
-            print("d_cut_index=", d_cut_index, ", d_cut=", d_cut)
+            d_cut = sk.index(max(sk))
+            #print("d_cut=", d_cut)
+            # cut x in offs_k at dimension d_cut
+            indx['d_cuts'].append(d_cut) 
+            # sort the x-value in offs_k at dimension d_cut, sv, soffs are lists
+            soffs, sv = zip(*sorted(enumerate(x_offs_k.T[d_cut]), key=itemgetter(1))) 
+            sv = list(sv)
+            soffs = list(soffs)
+            #print("sv=", sv)
+            #print("soffs=", soffs)
+            indx['v_cuts'].append(sv[moffs])
+            #print("indx=", indx)
+            # current parent node k, left kid would be 2k, right kid would be 2k+1
+            offs[2*k-1]     = [offs_k[_] for _ in soffs[1-1:moffs+1]]
+            offs[2*k+1-1]   = [offs_k[_] for _ in soffs[moffs+1+1-1:nk]] 
+            
+            # prompt
+            print("\n split [", k, ":", nk, "] at", d_cut, ": ", sv[moffs]) 
+                
+            # clean up node k
+            #offs[k-1] = []
+
+    # leaf nodes
+    leafs = [offs[2**ht+j-1] for j in range(nLeafNode)]
+    #print("leafs=", leafs)
     
-    return indx, offs #, leafs, mbrs
+    mbrs = [{"min": min([x[_][0] for _ in leafs[j]]), "max": max([x[_][0] for _ in leafs[j]])} for j in range(nLeafNode)]
+    #print("mbrs=", mbrs)
+    
+    return indx, leafs, mbrs
 
 
 
@@ -102,7 +124,9 @@ testing buildVisualWordList
 
 if __name__ == "__main__":
     
-    x = [[3, 4], [1, 2], [5, 6], [9, 10], [7, 8], [13, 14], [11, 12], [15, 16], [17, 18], [19, 20], [21, 22], [23, 24], [25, 26], [27, 28], [29, 30], [31, 32]]
-    ht = 3
-    indx, offs = buildVisualWordList(x, ht)
-
+    x = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18], [19, 20], [21, 22], [23, 24], [25, 26], [27, 28], [29, 30], [31, 32]]
+    ht = 4
+    indx, leafs, mbrs = buildVisualWordList(x, ht)
+    print("leafs=", leafs)
+    print("indx=", indx)
+    print("mbrs=", mbrs)
